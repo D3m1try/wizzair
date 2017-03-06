526c5c3bf1528f18c6e81fc6c990b8bbd0a37fbb
Aliaksandr_Krutsko <aliaksandr_krutsko@epam.com>
2017-03-06 16:56:00 +0300

    commit

--- src/main/java/com/epam/wizzair/page/impl/SearchResult.java
+++ src/main/java/com/epam/wizzair/page/impl/SearchResult.java
@@ -1,41 +1,43 @@
 package com.epam.wizzair.page.impl;
-
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import static com.epam.wizzair.driver.DriverSingleton.getDriver;
 
-
-public class SearchResult {
+public class SearchResult extends AbstractPage {
 
     @FindBy(xpath = "//*[@id=\"fare-selector-outbound\"]/div/table/tbody[5]/tr/td[2]/label/div/div/span")
     private WebElement firstFlight;
-    
+
     @FindBy(xpath = "//*[@id=\"fare-selector-return\"]/div/table/tbody[5]/tr/td[3]/label/div/div")
     private WebElement secondFlight;
-    
+
     @FindBy(xpath = "//*[@id=\"booking-flow\"]/aside/div[2]/div[5]/div/div[2]/span")
     private WebElement sum;
 
-    public SearchResult(){
 
-        PageFactory.initElements(getDriver(), this);
+    public SearchResult(WebDriver driver){
+        super(driver);
+        PageFactory.initElements(this.driver, this);
     }
 
     public String chooseFlights() {
-    	
-    	String firstPrice = firstFlight.getText();
-    	firstFlight.click();    	
-    	return firstPrice;
+
+        String firstPrice = firstFlight.getText();
+        firstFlight.click();
+        return firstPrice;
     }
-    
+
     public String getSum() {
-    	return sum.getText();
+        return sum.getText();
+    }
+
+    @Override
+    public void openPage() {
+
     }
 
 
-	
-	
-    
+
 }
--- src/main/java/com/epam/wizzair/step/TestCases.java
+++ src/main/java/com/epam/wizzair/step/TestCases.java
@@ -1,19 +1,31 @@
 package com.epam.wizzair.step;
 
-import com.epam.wizzair.driver.TestBase;
-import junit.framework.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
-public class TestCases extends TestBase {
+import junit.framework.Assert;
 
+
+public class TestCases {
+
+    private Steps steps;
+
+    @BeforeMethod(description = "Init browser")
+    public void setUp() {
+        steps = new Steps();
+        steps.initBrowser();
+    }
 
     @Test
-    public void flitsSum()  {
-        Steps steps = new Steps();
+    public void flitsSum() {
         steps.findFlight();
         Assert.assertEquals(steps.getFirstPrice(), steps.getSum());
     }
-    
 
+    @AfterMethod
+    public void close() {
+        steps.closeBrowser();
+    }
 }
